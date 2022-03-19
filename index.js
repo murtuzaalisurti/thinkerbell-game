631ee03a59ee3add704db026c75aab328076fd72
murtuza <murtuza.creativity@gmail.com>
2022-03-19 16:10:50 +0530

    testing..

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ const Words = require('./models/wordsModel');
 
 const express = require('express');
 const app = express();
-
-app.use(express.static(path.resolve(__dirname, './client/public')));
-
 const db_conn_uri = process.env.MONGOURI;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/public', 'index.html'));
+    res.send('server running');
 });
 
 app.get('/wordsData', (req, res) => {
@@ -33,3 +30,5 @@ app.get('/wordsData', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => console.log('listening'));
+
+module.exports = app;
