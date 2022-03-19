53a84321a66233d4b030bac5910157823aa604ba
murtuza <murtuza.creativity@gmail.com>
2022-03-19 10:43:42 +0530

    fixing prod issue ⚡

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/wordsData', (req, res) => {
     })
 })
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './client/public', 'index.html'));
 });
 
